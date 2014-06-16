77b502d2c6503216c9eb250d455e2bf5858607cd
Melvin Lervick <melvin.lervick@infospace.com>
2014-06-15 19:53:00 -0700

    Update Research Layout

--- BeyondSearch/MainWindow.xaml.cs
+++ BeyondSearch/MainWindow.xaml.cs
@@ -16,112 +16,112 @@ using System.Windows.Shapes;
 
 namespace BeyondSearch
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		private readonly object dummyNode = null;
-
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-
-		private void Menu_FileExitClick( object sender, RoutedEventArgs e )
-		{
-			Application app = Application.Current;
-			app.Shutdown();
-		}
-
-    private void Menu_ResearchClick( object sender, RoutedEventArgs e )
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      var researchPage = new Research();
-      researchPage.Show();
-    }
+        private readonly object dummyNode = null;
 
-    public string SelectedImagePath
-		{
-			get;
-			set;
-		}
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		private void Window_Loaded( object sender, RoutedEventArgs e )
-		{
-			foreach (string s in Directory.GetLogicalDrives())
-			{
-				var item = new TreeViewItem {Header = s, Tag = s, FontWeight = FontWeights.Normal};
-			    item.Items.Add( dummyNode );
-				item.Expanded += new RoutedEventHandler( folder_Expanded );
-				FoldersItem.Items.Add( item );
-			}
-		}
+        private void Menu_FileExitClick(object sender, RoutedEventArgs e)
+        {
+            Application app = Application.Current;
+            app.Shutdown();
+        }
 
-		void folder_Expanded( object sender, RoutedEventArgs e )
-		{
-			var item = (TreeViewItem)sender;
-			if (item.Items.Count == 1 && item.Items[0] == dummyNode)
-			{
-				item.Items.Clear();
-				try
-				{
-					foreach (string s in Directory.GetDirectories( item.Tag.ToString() ))
-					{
-						var subitem = new TreeViewItem
-						{
-						    Header = s.Substring(s.LastIndexOf("\\") + 1),
-						    Tag = s,
-						    FontWeight = FontWeights.Normal
-						};
-					    subitem.Items.Add( dummyNode );
-						subitem.Expanded += new RoutedEventHandler( folder_Expanded );
-						item.Items.Add( subitem );
-					}
-				}
-				catch (Exception)
-				{
-				}
-			}
-		}
+        private void Menu_ResearchClick(object sender, RoutedEventArgs e)
+        {
+            var researchPage = new Research();
+            researchPage.Show();
+        }
 
-		private void foldersItem_SelectedItemChanged( object sender, RoutedPropertyChangedEventArgs<object> e )
-		{
-			var tree = (TreeView)sender;
-			var temp = ((TreeViewItem)tree.SelectedItem);
+        public string SelectedImagePath
+        {
+            get;
+            set;
+        }
 
-			if (temp == null)
-				return;
-			SelectedImagePath = "";
-		    string temp2 = "";
-			while (true)
-			{
-				string temp1 = temp.Header.ToString();
-				if (temp1.Contains( @"\" ))
-				{
-					temp2 = "";
-				}
-				SelectedImagePath = temp1 + temp2 + SelectedImagePath;
-				if (temp.Parent.GetType() == typeof( TreeView ))
-				{
-					break;
-				}
-				temp = ((TreeViewItem)temp.Parent);
-				temp2 = @"\";
-			}
-			//show user selected path
-			//MessageBox.Show( SelectedImagePath );
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem { Header = s, Tag = s, FontWeight = FontWeights.Normal };
+                item.Items.Add(dummyNode);
+                item.Expanded += new RoutedEventHandler(folder_Expanded);
+                FoldersItem.Items.Add(item);
+            }
+        }
+
+        void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        var subitem = new TreeViewItem
+                        {
+                            Header = s.Substring(s.LastIndexOf("\\") + 1),
+                            Tag = s,
+                            FontWeight = FontWeights.Normal
+                        };
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var tree = (TreeView)sender;
+            var temp = ((TreeViewItem)tree.SelectedItem);
+
+            if (temp == null)
+                return;
+            SelectedImagePath = "";
+            string temp2 = "";
+            while (true)
+            {
+                string temp1 = temp.Header.ToString();
+                if (temp1.Contains(@"\"))
+                {
+                    temp2 = "";
+                }
+                SelectedImagePath = temp1 + temp2 + SelectedImagePath;
+                if (temp.Parent.GetType() == typeof(TreeView))
+                {
+                    break;
+                }
+                temp = ((TreeViewItem)temp.Parent);
+                temp2 = @"\";
+            }
+            //show user selected path
+            //MessageBox.Show( SelectedImagePath );
             if (this.BuildDirectoryAndFileList(SelectedImagePath))
             {
                 FolderList.ItemsSource = this.SelectedFolderDirectoryNamesAndFileNames;
             }
-		}
+        }
 
-	    public List<FolderItem> SelectedFolderDirectoryNamesAndFileNames { get; set; }
+        public List<FolderItem> SelectedFolderDirectoryNamesAndFileNames { get; set; }
         private bool BuildDirectoryAndFileList(string path)
         {
             DirectoryInfo filesAndDirectories = new DirectoryInfo(path);
             this.SelectedFolderDirectoryNamesAndFileNames = new List<FolderItem>();
-            
+
             this.AddDirectoryNamesToSelectedFolderItems(filesAndDirectories);
 
             this.AddFileNamesToSelectedFolderItems(filesAndDirectories);
@@ -129,22 +129,22 @@ namespace BeyondSearch
             return true;
         }
 
-	    private void AddFileNamesToSelectedFolderItems(DirectoryInfo filesAndDirectories)
-	    {
-	        FileSystemInfo[] files = filesAndDirectories.GetFiles();
-	        foreach (var file in files)
-	        {
-	            this.SelectedFolderDirectoryNamesAndFileNames.Add(new FolderItem(file.Name, file.Extension.Replace(".", "")));
-	        }
-	    }
+        private void AddFileNamesToSelectedFolderItems(DirectoryInfo filesAndDirectories)
+        {
+            FileSystemInfo[] files = filesAndDirectories.GetFiles();
+            foreach (var file in files)
+            {
+                this.SelectedFolderDirectoryNamesAndFileNames.Add(new FolderItem(file.Name, file.Extension.Replace(".", "")));
+            }
+        }
 
-	    private void AddDirectoryNamesToSelectedFolderItems(DirectoryInfo filesAndDirectories)
-	    {
-	        DirectoryInfo[] directories = filesAndDirectories.GetDirectories();
-	        foreach (var directory in directories)
-	        {
-	            this.SelectedFolderDirectoryNamesAndFileNames.Add(new FolderItem(directory.Name));
-	        }
-	    }
-	}
+        private void AddDirectoryNamesToSelectedFolderItems(DirectoryInfo filesAndDirectories)
+        {
+            DirectoryInfo[] directories = filesAndDirectories.GetDirectories();
+            foreach (var directory in directories)
+            {
+                this.SelectedFolderDirectoryNamesAndFileNames.Add(new FolderItem(directory.Name));
+            }
+        }
+    }
 }
--- BeyondSearch/Research.xaml.cs
+++ BeyondSearch/Research.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,159 +17,179 @@ using System.Windows.Shapes;
 
 namespace BeyondSearch
 {
-  /// <summary>
-  /// Interaction logic for Research.xaml
-  /// </summary>
-  public partial class Research : Window
-  {
-    private bool exactMatch = true;
-    private KeywordFilter filter = new KeywordFilter();
-
-    public Research()
+    /// <summary>
+    /// Interaction logic for Research.xaml
+    /// </summary>
+    public partial class Research : Window
     {
-      InitializeComponent();
-      InitializeKeywordList();
-      InitializeFilterList();
-    }
+        private bool exactMatch = true;
+        private KeywordFilter filter = new KeywordFilter();
 
-    private void InitializeKeywordList()
-    {
-      ListBoxKeywords.Items.Add( " hotels with pools " );
-      ListBoxKeywords.Items.Add( " hotels in south chicago red light " );
-      ListBoxKeywords.Items.Add( " stores that sell adult toys " );
-      ListBoxKeywords.Items.Add( " adult toys " );
-      ListBoxKeywords.Items.Add( " adult only restaurants " );
-
-      ListBoxKeywords.Items.Add( " animal shelter dog " );
-      ListBoxKeywords.Items.Add( " animal shelter dogs " );
-      ListBoxKeywords.Items.Add( " animal shelter cat " );
-      ListBoxKeywords.Items.Add( " animal shelter cats " );
-      ListBoxKeywords.Items.Add( " park zebra " );
-
-      ListBoxKeywords.Items.Add( " park zebras " );
-      ListBoxKeywords.Items.Add( " zoo animal zebra " );
-      ListBoxKeywords.Items.Add( " zoo animal zebras " );
-      ListBoxKeywords.Items.Add( " clothes young girls " );
-      ListBoxKeywords.Items.Add( " young girls " );
-
-      ListBoxKeywords.Items.Add( " zebra " );
-      ListBoxKeywords.Items.Add( " cat " );
-      ListBoxKeywords.Items.Add( " dog " );
-      ListBoxKeywords.Items.Add( " red light " );
-      ListBoxKeywords.Items.Add( " red lights " );
-    }
-
-    private void InitializeFilterList()
-    {
-      ListBoxFilters.Items.Add( "adult toys" );
-      ListBoxFilters.Items.Add( "zebra" );
-      ListBoxFilters.Items.Add( "young girls" );
-      ListBoxFilters.Items.Add( "red light" );
-      ListBoxFilters.Items.Add( "cat" );
-      
-      ListBoxFilters.Items.Add( "dog" );
-    }
-
-    private void Menu_FileExitClick( object sender, RoutedEventArgs e )
-    {
-      this.Close();
-    }
-
-    private void AddKeyword_Click( object sender, RoutedEventArgs e )
-    {
-      if ( TextBoxStringToAdd.Text.Length > 0 )
-      {
-        ListBoxKeywords.Items.Add( " " + TextBoxStringToAdd.Text + " " );
-      }
-    }
-
-    private void ClearKeyword_Click( object sender, RoutedEventArgs e )
-    {
-      ListBoxKeywords.Items.Clear();
-    }
-
-    private void AddFilter_Click( object sender, RoutedEventArgs e )
-    {
-      if (TextBoxStringToAdd.Text.Length > 0)
-      {
-        ListBoxFilters.Items.Add( TextBoxStringToAdd.Text );
-      }
-    }
-
-    private void ClearFilter_Click( object sender, RoutedEventArgs e )
-    {
-      ListBoxFilters.Items.Clear();
-    }
-
-    private void FilterType_Checked( object sender, RoutedEventArgs e )
-    {
-      var button = sender as RadioButton;
-      exactMatch = false || button != null && button.Content.ToString() == "Exact";
-    }
-
-    private void Filter_Click( object sender, RoutedEventArgs e )
-    {
-      var sw = new Stopwatch();
-      ListBoxFilteredKeywords.Items.Clear();
-      var keywords = new List<string>();
-      var filters = new List<string>();
-
-      if ( exactMatch )
-      {
-        filters = ListBoxFilters.Items.Cast<string>().ToList();
-        filter.FillExactFilterList( DuplicateList(filters,100) );
-        if (ListBoxKeywords.Items.Count > 0)
+        public Research()
         {
-          keywords = ListBoxKeywords.Items.Cast<string>().ToList();
-          sw.Start();
-          var filteredItems = filter.Exact( DuplicateList( keywords, 500 ) );
-          sw.Stop();
-          foreach (var filteredItem in filteredItems)
-          {
-            ListBoxFilteredKeywords.Items.Add( filteredItem );
-          }
-          TextBoxElapsed.Text = sw.ElapsedMilliseconds.ToString();
+            InitializeComponent();
+            InitializeKeywordList();
+            InitializeFilterList();
         }
-      }
-      else
-      {
-        filters = ListBoxFilters.Items.Cast<string>().ToList();
-        filter.FillContainsFilterList( DuplicateList(filters,100) );
-        if (ListBoxKeywords.Items.Count > 0)
+
+        private void InitializeKeywordList()
         {
-          keywords = ListBoxKeywords.Items.Cast<string>().ToList();
-          sw.Start();
-          var filteredItems = filter.Contains( DuplicateList(keywords,500) );
-          sw.Stop();
-          foreach (var filteredItem in filteredItems)
-          {
-            ListBoxFilteredKeywords.Items.Add( filteredItem );
-          }
-          TextBoxElapsed.Text = sw.ElapsedMilliseconds.ToString();
+            ListBoxKeywords.Items.Add(" hotels with pools ");
+            ListBoxKeywords.Items.Add(" hotels in south chicago red light ");
+            ListBoxKeywords.Items.Add(" stores that sell adult toys ");
+            ListBoxKeywords.Items.Add(" adult toys ");
+            ListBoxKeywords.Items.Add(" adult only restaurants ");
+
+            ListBoxKeywords.Items.Add(" animal shelter dog ");
+            ListBoxKeywords.Items.Add(" animal shelter dogs ");
+            ListBoxKeywords.Items.Add(" animal shelter cat ");
+            ListBoxKeywords.Items.Add(" animal shelter cats ");
+            ListBoxKeywords.Items.Add(" park zebra ");
+
+            ListBoxKeywords.Items.Add(" park zebras ");
+            ListBoxKeywords.Items.Add(" zoo animal zebra ");
+            ListBoxKeywords.Items.Add(" zoo animal zebras ");
+            ListBoxKeywords.Items.Add(" clothes young girls ");
+            ListBoxKeywords.Items.Add(" young girls ");
+
+            ListBoxKeywords.Items.Add(" zebra ");
+            ListBoxKeywords.Items.Add(" cat ");
+            ListBoxKeywords.Items.Add(" dog ");
+            ListBoxKeywords.Items.Add(" red light ");
+            ListBoxKeywords.Items.Add(" red lights ");
         }
-      }
+
+        private void InitializeFilterList()
+        {
+            ListBoxFilters.Items.Add("adult toys");
+            ListBoxFilters.Items.Add("zebra");
+            ListBoxFilters.Items.Add("young girls");
+            ListBoxFilters.Items.Add("red light");
+            ListBoxFilters.Items.Add("cat");
+
+            ListBoxFilters.Items.Add("dog");
+        }
+
+        private void Menu_FileExitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddKeyword_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxStringToAdd.Text.Length > 0)
+            {
+                ListBoxKeywords.Items.Add(" " + TextBoxStringToAdd.Text + " ");
+            }
+        }
+
+        private void ClearKeyword_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxKeywords.Items.Clear();
+        }
+
+        private void AddFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxStringToAdd.Text.Length > 0)
+            {
+                ListBoxFilters.Items.Add(TextBoxStringToAdd.Text);
+            }
+        }
+
+        private void ClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxFilters.Items.Clear();
+        }
+
+        private void FilterType_Checked(object sender, RoutedEventArgs e)
+        {
+            var button = sender as RadioButton;
+            exactMatch = false || button != null && button.Content.ToString() == "Exact";
+        }
+
+        private void Filter_Click(object sender, RoutedEventArgs e)
+        {
+            var sw = new Stopwatch();
+            ListBoxFilteredKeywords.Items.Clear();
+            var keywords = new List<string>();
+            var filters = new List<string>();
+
+            if (exactMatch)
+            {
+                filters = ListBoxFilters.Items.Cast<string>().ToList();
+                filter.FillExactFilterList(DuplicateList(filters, 100));
+                if (ListBoxKeywords.Items.Count > 0)
+                {
+                    keywords = ListBoxKeywords.Items.Cast<string>().ToList();
+                    sw.Start();
+                    var filteredItems = filter.Exact(DuplicateList(keywords, 500));
+                    sw.Stop();
+                    foreach (var filteredItem in filteredItems)
+                    {
+                        ListBoxFilteredKeywords.Items.Add(filteredItem);
+                    }
+                    TextBoxElapsed.Text = sw.ElapsedMilliseconds.ToString();
+                }
+            }
+            else
+            {
+                filters = ListBoxFilters.Items.Cast<string>().ToList();
+                filter.FillContainsFilterList(DuplicateList(filters, 100));
+                if (ListBoxKeywords.Items.Count > 0)
+                {
+                    keywords = ListBoxKeywords.Items.Cast<string>().ToList();
+                    sw.Start();
+                    var filteredItems = filter.Contains(DuplicateList(keywords, 500));
+                    sw.Stop();
+                    foreach (var filteredItem in filteredItems)
+                    {
+                        ListBoxFilteredKeywords.Items.Add(filteredItem);
+                    }
+                    TextBoxElapsed.Text = sw.ElapsedMilliseconds.ToString();
+                }
+            }
+        }
+
+        private IEnumerable<string> DuplicateList(List<string> list, int noTimesToDuplicate)
+        {
+            var listToReturn = new List<string>();
+
+            for (int i = 0; i < noTimesToDuplicate; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (i > 0)
+                    {
+                        listToReturn.Add(list[j] + i.ToString() + " " + j.ToString() + " ");
+                    }
+                    else
+                    {
+                        listToReturn.Add(list[j]);
+                    }
+                }
+            }
+
+            return listToReturn;
+        }
+
+        private void ButtonKeywordsFile_Click(object sender, RoutedEventArgs routedEventArgs)
+        {
+            // Create OpenFileDialog 
+            var dlg = new Microsoft.Win32.OpenFileDialog {DefaultExt = ".txt", Filter = "Text documents (.txt)|*.txt"};
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            var result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                TextBoxKeywordFile.Text = dlg.FileName;
+            }
+        }
+
+        private void ButtonFilterFile_Click( object sender, RoutedEventArgs e )
+        {
+        }
     }
-
-    private IEnumerable<string> DuplicateList( List<string> list, int noTimesToDuplicate )
-    {
-      var listToReturn = new List<string>();
-
-      for ( int i = 0; i < noTimesToDuplicate; i++ )
-      {
-        for ( int j = 0; j < list.Count; j++ )
-        {
-          if ( i > 0 )
-          {
-            listToReturn.Add( list[j] + i.ToString() + " " + j.ToString() + " " );
-          }
-          else
-          {
-            listToReturn.Add( list[j] );
-          }
-        }
-      }
-
-      return listToReturn;
-    } 
-  }
 }
