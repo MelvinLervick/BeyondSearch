b65d368b1a81e994d9149b1c9f3fed31b176c62f
Melvin Lervick <melvin.lervick@infospace.com>
2014-07-16 19:50:05 -0700

    Update Interface

--- BeyondSearch/IKeywordFilter.cs
+++ BeyondSearch/IKeywordFilter.cs
@@ -3,13 +3,23 @@ using System.Windows.Controls;
 
 namespace BeyondSearch
 {
-  interface IKeywordFilter
-  {
-    List<string> FilterList { get; }
+    interface IKeywordFilter
+    {
+        List<string> FilterList { get; }
 
-    List<string> Exact( IEnumerable<string> keywords );
-    List<string> StrictContains( IEnumerable<string> keywords );
+        List<string> Contains(IEnumerable<string> keywords);
+        List<string> Contains1(IEnumerable<string> keywords);
+        List<string> Exact(IEnumerable<string> keywords);
+        List<string> Exact1(IEnumerable<string> keywords);
+        List<string> StrictContains(IEnumerable<string> keywords);
+        List<string> StrictContains1(IEnumerable<string> keywords);
+        List<string> ContainsSansSpaceAndNumber(IEnumerable<string> keywords);
+        List<string> ContainsSansSpaceAndNumber1(IEnumerable<string> keywords);
+        List<string> FuzzyContains( IEnumerable<string> keywords );
+        List<string> FuzzyContains1( IEnumerable<string> keywords );
+        List<string> LucenePorterStemContains( IEnumerable<string> keywords );
+        List<string> LucenePorterStemContains1( IEnumerable<string> keywords );
 
-    void FillFilterList( IEnumerable<string> filters );
-  }
+        void FillFilterList(IEnumerable<string> filters);
+    }
 }
